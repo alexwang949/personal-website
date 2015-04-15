36c4ac2b533bd8d76511c93db88223bb8368843a
alexwang949 <alex_cellist@yahoo.com>
2015-04-15 15:04:59 -0400

    added basic layout section dividers, added scroll test for nav

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,58 +1,50 @@
-// console.log('linked!');
+console.log('linked!');
 
-// $('button').first().click(function() {
-// 	$.smoothScroll({
-// 		scrollTarget: $('#p3'),
-// 		speed: 1000
-// 	});
-// });
-
-// $('button:nth-of-type(2)').click(function() {
-// 	$.smoothScroll({
-// 		scrollTarget: $('#p1')
-// 	});
-// });
 
 window.onload = function() {
 
-
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navBarHeight = $('nav-div').outerHeight();
-
-
-    $(window).scroll(function(event){
-        didScroll = true;
+    $('#contact-button').click(function() {
+        $.smoothScroll({
+            scrollTarget: $('.contact-section'),
+            speed: 1000
+        });
     });
 
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        };
-    }, 250);
+        var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navBarHeight = $('nav-div').outerHeight();
 
-    function hasScrolled() {
-        var st = $(this).scrollTop();
 
-        if (Math.abs(lastScrollTop - st) <= delta) {
-            return;
-        };
+        $(window).scroll(function(event){
+            didScroll = true;
+        });
 
-        if (st > lastScrollTop && st > navBarHeight) {
-            $('.nav-div').removeClass('nav-down').addClass('nav-up');
-        } else {
-                $('.nav-div').removeClass('nav-up').addClass('nav-down');
-        };
+        setInterval(function() {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            };
+        }, 250);
 
-        lastScrollTop = st;
+        function hasScrolled() {
+            var st = $(this).scrollTop();
 
-    }
+            if (Math.abs(lastScrollTop - st) <= delta) {
+                return;
+            };
+
+            if (st > lastScrollTop && st > navBarHeight) {
+                $('.nav-div').removeClass('nav-down').addClass('nav-up');
+            } else {
+                    $('.nav-div').removeClass('nav-up').addClass('nav-down');
+            };
+
+            lastScrollTop = st;
+
+        }
 
    
-
-
 };
 
 
